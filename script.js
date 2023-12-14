415d5c99fd96d5b08b2026dea55c964432b618ae
kway-zar <137285693+kway-zar@users.noreply.github.com>
2023-12-14 21:39:39 +0800

    ehe

--- script.js
+++ script.js
@@ -3,14 +3,25 @@
 const logoButton = document.querySelector('.logoButton');
 
 fade("rgb(85,0,170)","40%");
+var word;
 
-logoButton.addEventListener("onclick",);
+logoButton.addEventListener("mouseover",function(){
+    // const rect = logoButton.getBoundingClientRect();
+    // const x = rect.left + window.scrollX;
+    // const y = rect.top + window.scrollY;
 
+    lines = document.querySelector(".body .lines");
+    X_px = x + "px";
+    Y_px = y + "px";
+    word = "translate(" + X_px + "," + Y_px + ")";
 
-function makeLines() {
+    lines.style.transform = word;
+    lines.style.transform = "rotate(10deg)";
 
+});
 
-}
+console.log(word);
+
 
 function fade(COLOUR, OPACITY) {
 
